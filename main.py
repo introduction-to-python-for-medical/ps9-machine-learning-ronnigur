e8444918db82410f3a8217069c9f48a5a28c08dc
ronnigur <rgwr3719@gmail.com>
2025-01-06 19:39:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 %load_ext autoreload
 %autoreload 2
+
 # Download the data from your GitHub repository
 !wget https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv -O /content/parkinsons.csv
 import pandas as pd
@@ -8,30 +9,24 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, classification_report
 import matplotlib.pyplot as plt
-
-# Load the dataset
 file_path = '/content/parkinsons.csv'  # Update with your file path
 df = pd.read_csv(file_path)
 df = df.dropna()
-
 # Define features (DFA and PPE) and target (status)
 X = df[['DFA', 'PPE']]  # Replace with exact column names for HNR and PPE
 y = df['status']  # Target column (1 = Parkinson's, 0 = Healthy)
-
-# Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Scale the features (DFA and PPE) for better model performance
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-# Train a K-Nearest Neighbors (KNN) Classifier
 knn = KNeighborsClassifier(n_neighbors=7)  # Use 5 neighbors
 knn.fit(X_train_scaled, y_train)
 
-# Make predictions
 y_pred = knn.predict(X_test_scaled)
 
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Accuracy: {accuracy}")
+import joblib
+
+joblib.dump(knn, 'my_model.joblib')
